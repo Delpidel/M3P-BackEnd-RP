af505e3693247a662b8e39fd11e1d9757aa511ba
Henrique Douglas <henriquedouglas2803@gmail.com>
2024-03-12 20:30:20 -0300

    feat: adiciona rota de estudante

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('students', [StudentController::class, 'index']);
 });
 
 Route::post('login', [AuthController::class, 'store']);
