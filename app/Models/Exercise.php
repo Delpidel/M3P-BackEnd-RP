7d4174de610a50580b9c84747ebf0c88f1edffab
Drew <84882983+vdr3w@users.noreply.github.com>
2024-03-16 16:20:14 -0300

    feat: Update Student and Exercise models with user relationship
    Atualiza os modelos Student e Exercise para incluir a relação com o modelo User. Cada estudante e exercício agora pode ser associado a um usuário específico.

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -9,5 +9,10 @@ class Exercise extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['description'];
+    protected $fillable = ['description', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
