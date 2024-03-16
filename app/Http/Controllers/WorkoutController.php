e12d5d84063fa14a7f77f1e2023551fb709b430b
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2024-03-16 00:11:24 -0500

    feat:join con tablea exercises para mostrar a description do exercise

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -25,8 +25,9 @@ class WorkoutController extends Controller
     public function workoutsByStudent($studentId)
     {
         try {
-            $workouts = Workout::select('workouts.*', 'students.name')
+            $workouts = Workout::select('workouts.*', 'students.name', 'exercises.description')
             ->join('students', 'workouts.student_id', '=', 'students.id')
+            ->join('exercises', 'workouts.exercise_id', '=', 'exercises.id')
             ->where('workouts.student_id', $studentId)
             ->orderBy('created_at')
             ->get();
