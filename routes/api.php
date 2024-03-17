ef4cf7e92f4ccbdf9896d2f75128dfc8b0ff5631
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2024-03-17 12:07:51 -0300

    feat: create student document request
    Create a private route and a POST request.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\StudentDocumentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('login', [AuthController::class, 'store']);
+
+Route::middleware('auth')->post('/students/{id}/documents', [StudentDocumentController::class, 'store']);
+
