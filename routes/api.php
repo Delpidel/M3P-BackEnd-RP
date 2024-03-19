17167da97ef0ac386cb3dc061716a2310b130111
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-03-18 21:09:28 -0300

    refactor: inclui a rota de post dentro do middleware

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'store']);
 
+
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('students', [StudentController::class, 'store'])->middleware(['ability:create-students']);
+    Route::post('students', [StudentController::class, 'store']);
 });
