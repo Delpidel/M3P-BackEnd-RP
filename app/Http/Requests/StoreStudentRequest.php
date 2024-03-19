4d4108ff7913df1f1368af98d68125f671e9cfe9
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-03-18 21:36:06 -0300

    feat: cria arquivo de request do cadastro de estudante

---
+++ app/Http/Requests/StoreStudentRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreStudentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'string|required|max:255',
+            'email' => 'string|required|email|max:255|unique:students',
+            'date_birth' => 'nullable|date_format:Y-m-d',
+            'contact' => 'string|required|max:20',
+            'cpf' => 'nullable|unique:students|regex:/^(?:\d{3}\.\d{3}\.\d{3}-\d{2}|\d{11})$/',
+            'cep' => 'nullable|string|max:20',
+            'street' => 'required|string',
+            'state' => 'required|string|max:2',
+            'neighborhood' => 'required|string',
+            'city' => 'required|string',
+            'number' => 'required|string',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'O campo nome é obrigatório.',
+            'name.max' => 'O campo nome deve ter no máximo 255 caracteres.',
+            'email.required' => 'O campo email é obrigatório.',
+            'email.email' => 'O campo email deve ser um endereço de e-mail válido.',
+            'email.max' => 'O campo email deve ter no máximo 255 caracteres.',
+            'email.unique' => 'O email informado já está em uso.',
+            'date_birth.date_format' => 'O campo data de nascimento deve estar no formato Ano-Mês-Dia.',
+            'contact.required' => 'O campo contato é obrigatório.',
+            'contact.max' => 'O campo contato deve ter no máximo 20 caracteres.',
+            'cpf.regex' => 'O campo CPF deve estar no formato válido.',
+            'cep.max' => 'O campo CEP deve ter no máximo 20 caracteres.',
+        ];
+    }
+}
