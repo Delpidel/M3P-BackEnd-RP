2df0727602eade66524093fc49074f8d512ddbb3
joseCdia <josecdia@hotmail.com>
2024-03-19 01:34:35 -0300

    Adicionado rota no arquivo composer.json para os arquivos  que não estavam sendo encontrados

---
+++ app/Http/Requests/UpdateWorkoutRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UpdateWorkoutRequest extends FormRequest
+{
+
+    protected $stopOnFirstFailure = true;
+
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'repetitions' => 'integer',
+            'weight' => 'numeric',
+            'break_time' => 'integer',
+            'day' => 'in:SEGUNDA,TERÇA,QUARTA,QUINTA,SEXTA,SÁBADO,DOMINGO',
+            'observations' => 'string',
+            'time' => 'integer',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'repetitions.integer' => 'O número de repetições deve ser um número inteiro',
+            'weight.numeric' => 'O peso deve ser um número decimal',
+            'break_time.integer' => 'O tempo de pausa entre as séries deve ser um número inteiro',
+            'day.in' => 'O dia da semana deve ser um dos seguintes: SEGUNDA, TERÇA, QUARTA, QUINTA, SEXTA, SÁBADO, DOMINGO',
+            'observations.string' => 'As observações devem ser uma string',
+            'time.integer' => 'O tempo deve ser um número inteiro',
+        ];
+    }
+}
