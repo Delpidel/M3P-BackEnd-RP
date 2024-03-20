88fa016dc7627b812b6e58449b12fd4c5fd9a22c
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2024-03-20 20:01:06 -0300

    refactor: use dependence injection

--- app/Http/Controllers/StudentDocumentController.php
+++ app/Http/Controllers/StudentDocumentController.php
@@ -2,31 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Student;
 use App\Models\StudentDocument;
 use Illuminate\Http\Request;
+use App\Http\Services\File\CreateFileService;
 use Symfony\Component\HttpFoundation\Response;
 
 class StudentDocumentController extends Controller
 {
+    private $createFileService;
+
+    public function __construct(CreateFileService $createFileService)
+    {
+        $this->createFileService = $createFileService;
+    }
+
     public function store(Request $request)
     {
         try {
             $data = $request->validate([
                 'title' => 'nullable|string|max:255',
-                'file' => 'nullable|string|max:255|unique:student_documents,file',
+                'file_id' => 'required|exists:files,id',
             ]);
 
             $studentId = auth()->user()->id;
 
+            // Create student document record in the database
             $document = StudentDocument::create([
                 'title' => $data['title'],
-                'file' => $data['file'],
+                'file_id' => $data['file_id'],
                 'student_id' => $studentId,
             ]);
 
             return response()->json(['message' => 'Document created successfully', 'document' => $document], Response::HTTP_OK);
-
         } catch (\Exception $exception) {
             return response()->json(['error' => $exception->getMessage()], Response::HTTP_BAD_REQUEST);
         }
