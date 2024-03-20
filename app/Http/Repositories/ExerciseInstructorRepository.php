308b5bf69acef86c4e1d18c8380fb768e86af360
Drew <84882983+vdr3w@users.noreply.github.com>
2024-03-20 16:50:39 -0300

    feat: Integrate Pagination Service in Exercise Repository
    Integra o PaginationInstructorService no ExerciseInstructorRepository para centralizar a lógica de paginação.

--- app/Http/Repositories/ExerciseInstructorRepository.php
+++ app/Http/Repositories/ExerciseInstructorRepository.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Repositories;
 
+use App\Http\Services\PaginationInstructorService;
 use App\Interfaces\ExerciseInstructorRepositoryInterface;
 use App\Models\Exercise;
 
 class ExerciseInstructorRepository implements ExerciseInstructorRepositoryInterface
 {
+    protected $paginationService;
+
+    public function __construct(PaginationInstructorService $paginationService)
+    {
+        $this->paginationService = $paginationService;
+    }
+
     public function getUserExercises($userId)
     {
-        return Exercise::where('user_id', $userId)->paginate(10, ['id', 'description']);
+        $query = Exercise::where('user_id', $userId);
+
+        // Referente a testes: Mudar esse valor '10', afeta o número de itens na página.
+        return $this->paginationService->paginate($query, 10, ['id', 'description']);
+
     }
 }
