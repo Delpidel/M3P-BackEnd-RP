bac7be6dd792088c0ebfa27eb4d04393d971a237
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-03-20 22:22:26 -0300

    refactor: ajuste erro de sintaxe

--- app/Mail/CredentialsStudent.php
+++ app/Mail/CredentialsStudent.php
@@ -1,61 +1,62 @@
-<<?php
+<?php
 
-    namespace App\Mail;
-
-    use Illuminate\Bus\Queueable;
-    use Illuminate\Contracts\Queue\ShouldQueue;
-    use Illuminate\Mail\Mailable;
-    use Illuminate\Mail\Mailables\Content;
-    use Illuminate\Mail\Mailables\Envelope;
-    use Illuminate\Queue\SerializesModels;
+namespace App\Mail;
 
 
-    class CredentialsStudent extends Mailable
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+
+class CredentialsStudent extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $student;
+    public $password;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($student, $password)
     {
-        use Queueable, SerializesModels;
-
-        public $student;
-        public $password;
-
-        /**
-         * Create a new message instance.
-         *
-         * @return void
-         */
-        public function __construct($student, $password)
-        {
-            $this->student = $student;
-            $this->password = $password;
-        }
-
-        /**
-         * Get the message envelope.
-         */
-        public function envelope(): Envelope
-        {
-            return new Envelope(
-                subject: 'Envio de credencias de acesso do estudante',
-            );
-        }
-
-        /**
-         * Get the message content definition.
-         */
-        public function content(): Content
-        {
-            return new Content(
-                html: 'mails.credentialsStudentTemplate',
-            );
-        }
-
-        /**
-         * Get the attachments for the message.
-         *
-         * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-         */
-        public function attachments(): array
-        {
-            return [];
-        }
+        $this->student = $student;
+        $this->password = $password;
     }
-    ?>
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Envio de credencias de acesso do estudante',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            html: 'mails.credentialsStudentTemplate',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
