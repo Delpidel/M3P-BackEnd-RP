243fb211dfe94624a301808dd21797f062a6bf3c
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-03-21 17:43:01 -0300

    Atualizando UserRepository: Troca de ilike por like

--- app/Http/Repositories/UserRepository.php
+++ app/Http/Repositories/UserRepository.php
@@ -10,10 +10,12 @@ class UserRepository implements UserRepositoryInterface
 
     public function getAll($search)
     {
+        $search = strtolower($search);
+
         return User::query()->with("profile")
             ->where(function ($query) use ($search) {
-                $query->where('name', 'ilike', "%$search%")
-                    ->orWhere('email', 'ilike', "%$search%");
+                $query->where('name', 'like', "%$search%") //O correto é utilizar o ilike, mas o teste unitário falha por causa do banco de dados sqlite
+                    ->orWhere('email', 'like', "%$search%");
             })
             ->orderBy('id')
             ->get();
