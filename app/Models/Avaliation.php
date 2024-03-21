05c3c5015def5d347a82a7311741e34de9739249
ThiagoRiosB <123420372+ThiagoRiosB@users.noreply.github.com>
2024-03-21 11:29:26 -0300

    refactor: Ajustei a migration avaliations.

---
+++ app/Models/Avaliation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Avaliation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['student_id', 'date', 'weight', 'height', 'age', 'observations_to_student', 'observations_to_nutritionist', 'file_id', ];
+
+    public function Student() {
+        return $this->hasOne(File::class, 'id', 'student_id');
+    }
+
+    public function file() {
+        return $this->hasOne(File::class, 'id', 'file_id');
+    }
+}
--- database/migrations/2024_03_20_103738_create_avaliations_table.php
+++ database/migrations/2024_03_20_103738_create_avaliations_table.php
@@ -21,21 +21,7 @@ class CreateAvaliationsTable extends Migration
             $table->text('observations_to_nutritionist')->nullable();
             $table->unsignedBigInteger('file_id');
             $table->foreign('file_id')->references('id')->on('files');
-            $table->integer('torax');
-            $table->integer('braco_direito');
-            $table->integer('braco_esquerdo');
-            $table->integer('cintura');
-            $table->integer('antebraco_esquerdo');
-            $table->integer('antebraco_direito');
-            $table->integer('abdome');
-            $table->integer('coxa_direita');
-            $table->integer('coxa_esquerda');
-            $table->integer('quadril');
-            $table->integer('panturrilha_direita');
-            $table->integer('panturilha_esquerda');
-            $table->integer('punho');
-            $table->integer('b_femoral_direito');
-            $table->integer('b_femoral_esquerdo');
+            $table->json('measures');
             $table->timestamps();
 
 
