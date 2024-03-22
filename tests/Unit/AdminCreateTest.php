6dafd6b28c1e0c4f82cb7ce538b3bf8fc5690eb1
Caroline Sampaio <caroline_08022@hotmail.com>
2024-03-22 15:00:35 -0300

    feat: teste admin pode criar recepcionista com imagem

--- tests/Unit/AdminCreateTest.php
+++ tests/Unit/AdminCreateTest.php
@@ -25,4 +25,25 @@ class AdminCreateTest extends TestCase
 
         $response->assertStatus(201)->assertJsonStructure(['id', 'name', 'email', 'profile_id', 'created_at', 'updated_at']);
     }
+
+    public function test_admin_can_create_user_recepcionista_with_image()
+    {
+        $user = User::factory()->create(['profile_id' => 1]);
+        $token = $user->createToken('@academia', ['create-users'])->plainTextToken;
+
+        Storage::fake('s3');
+
+        $recepcionista = [
+            'name' => 'Recepcionista',
+            'email' => 'recep@test.com',
+            'profile_id' => 2,
+            'password' => '12345678',
+            'photo' => UploadedFile::fake()->image('recepcionista.jpg') // Mock do upload do arquivo
+        ];
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)->post('/api/users', $recepcionista);
+
+        $response->assertStatus(201)
+            ->assertJsonStructure(['id', 'name', 'email', 'profile_id', 'file_id', 'created_at', 'updated_at']);
+    }
 }
