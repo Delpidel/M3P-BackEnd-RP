f8272f026e56c6e4ed4966c300997af7342bbc8e
Caroline Sampaio <caroline_08022@hotmail.com>
2024-03-22 15:21:33 -0300

    feat: adiciona testes para criacao de usuario instrutor
    admin pode criar um usuário instrutor e  pode criar com imagem.
    outros users nao tem permissao para criar

--- tests/Unit/AdminCreateTest.php
+++ tests/Unit/AdminCreateTest.php
@@ -77,4 +77,71 @@ class AdminCreateTest extends TestCase
 
         $response->assertStatus(403)->assertJson(['message' => 'Acesso negado. Você não tem permissão para executar esta ação.']);
     }
+
+    public function test_admin_can_create_user_instrutor()
+    {
+        Mail::fake();
+
+        $user = User::factory()->create(['profile_id' => 1]);
+        $token = $user->createToken('@academia', ['create-users'])->plainTextToken;
+
+        $instrutor = [
+            'name' => 'Instrutor',
+            'email' => 'instrutor@test.com',
+            'profile_id' => 3,
+            'password' => '12345678',
+        ];
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)->post('/api/users', $instrutor);
+
+        Mail::assertSent(SendWelcomeToNewUser::class, function ($mail) {
+            return $mail->hasTo('instrutor@test.com');
+        });
+
+        $response->assertStatus(201)->assertJsonStructure(['id', 'name', 'email', 'profile_id', 'created_at', 'updated_at']);
+    }
+
+    public function test_admin_can_create_user_instrutor_with_image()
+    {
+        Mail::fake();
+
+        $user = User::factory()->create(['profile_id' => 1]);
+        $token = $user->createToken('@academia', ['create-users'])->plainTextToken;
+
+        Storage::fake('s3'); // Mock AWS S3
+
+        $instrutor = [
+            'name' => 'Instrutor',
+            'email' => 'instrutor@test.com',
+            'profile_id' => 3,
+            'password' => '12345678',
+            'photo' => UploadedFile::fake()->image('instrutor.jpg') // Mock do upload do arquivo
+        ];
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)->post('/api/users', $instrutor);
+
+        Mail::assertSent(SendWelcomeToNewUser::class, function ($mail) {
+            return $mail->hasTo('instrutor@test.com');
+        });
+
+        $response->assertStatus(201)
+            ->assertJsonStructure(['id', 'name', 'email', 'profile_id', 'file_id', 'created_at', 'updated_at']);
+    }
+
+    public function test_others_users_cannot_create_user_instrutor()
+    {
+        $user = User::factory()->create(['profile_id' => 3]);
+        $token = $user->createToken('@academia', [''])->plainTextToken;
+
+        $instrutor = [
+            'name' => 'Recepcionista',
+            'email' => 'recep@test.com',
+            'profile_id' => 2,
+            'password' => '12345678',
+        ];
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)->post('/api/users', $instrutor);
+
+        $response->assertStatus(403)->assertJson(['message' => 'Acesso negado. Você não tem permissão para executar esta ação.']);
+    }
 }
