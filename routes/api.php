13062c906e7ca774ae0ceb26c86437a99f2697e4
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-03-23 20:49:56 -0300

    feat: inclui ability na rota de post

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('students', [StudentController::class, 'index'])->middleware(['ability:get-students']);
-    Route::post('students', [StudentController::class, 'store']);
+    Route::post('students', [StudentController::class, 'store'])->middleware(['ability:create-students']);
     Route::get('workouts', [WorkoutController::class, 'index'])->middleware(['ability:get-workouts']);
     Route::post('logout', [AuthController::class, 'logout']);
 });
