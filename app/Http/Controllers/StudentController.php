1a71511324c884e812b9270b4ad734863d5b188f
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-03-23 20:51:25 -0300

    feat: implementa os services criados para criação de estudante, gerar senha aleatoria, envio de email, criação de user e criação de userstudent

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,21 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreStudentRequest;
+
 use App\Http\Services\File\CreateFileService;
-
-use App\Mail\CredentialsStudent;
-
+use App\Http\Services\Student\CreateOneStudentService;
+use App\Http\Services\Student\PasswordGenerationService;
+use App\Http\Services\Student\PasswordHashingService;
+use App\Http\Services\Student\SendCredentialsStudentEmail;
+use App\Http\Services\User\CreateOneUserService;
+use App\Http\Services\UserStudent\CreateOneUserStudentService;
 
 use App\Models\Student;
-use App\Models\User;
-use App\Models\UserStudent;
-
-use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Mail;
 
 use App\Traits\HttpResponses;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
+
 use Symfony\Component\HttpFoundation\Response;
 
 class StudentController extends Controller
@@ -35,44 +33,35 @@ class StudentController extends Controller
         }
     }
 
-    public function store(StoreStudentRequest $request, CreateFileService $createFileService)
-    {
-        try { // remover no ultimo commit
+    public function store(
+        StoreStudentRequest $request,
+        CreateFileService $createFileService,
+        CreateOneStudentService $createOneStudentService,
+        PasswordGenerationService $passwordGenerationService,
+        PasswordHashingService $passwordHashingService,
+        SendCredentialsStudentEmail $sendCredentialsStudentEmail,
+        CreateOneUserService $createOneUserService,
+        CreateOneUserStudentService $createOneUserStudentService
+    ) {
 
-            DB::beginTransaction(); // remover no ultimo commit
+        $file = $request->file('photo');
 
-            $file = $request->file('photo');
+        $body = $request->all();
 
-            $body = $request->all();
+        $file = $createFileService->handle('photos', $file, $body['name']);
 
-            $file = $createFileService->handle('photos', $file, $body['name']);
+        $password = $passwordGenerationService->handle();
 
-            $password = Str::password(8);
+        $passwordHashed = $passwordHashingService->handle($password);
 
-            $passwordHashed = Hash::make($password);
+        $student = $createOneStudentService->handle([...$body, 'file_id' => $file->id]);
 
-            $student = Student::create([...$body, 'file_id' => $file->id]);
+        $user = $createOneUserService->handle([...$body, 'password' => $passwordHashed]);
 
-            $user = User::create([
-                'name' => $body['name'],
-                'email' => $body['email'],
-                'password' => $passwordHashed,
-                'profile_id' => 5,
-            ]);
+        $createOneUserStudentService->handle(['user_id' => $user->id, 'student_id' => $student->id]);
 
-            UserStudent::create([
-                'user_id' => $user->id,
-                'student_id' => $student->id,
-            ]);
+        $sendCredentialsStudentEmail->handle($student, $password);
 
-            Mail::to($student->email)->send(new CredentialsStudent($student, $password));
-
-            DB::commit();
-
-            return $student;
-        } catch (\Exception $exception) {
-            DB::rollBack();
-            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
-        }
+        return $student;
     }
 }
