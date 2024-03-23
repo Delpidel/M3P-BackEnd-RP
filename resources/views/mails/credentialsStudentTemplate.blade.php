f9d9f0b0b7a50c438db8479c4c7ba8ec29569625
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-03-23 12:25:56 -0300

    refactor: inclui acento que estava faltando na palavra credências

--- resources/views/mails/credentialsStudentTemplate.blade.php
+++ resources/views/mails/credentialsStudentTemplate.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crendenciais de acesso</title>
+    <title>Crendênciais de acesso</title>
     <style>
         body {
             margin: 0;
