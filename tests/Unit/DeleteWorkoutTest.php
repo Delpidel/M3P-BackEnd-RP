755d621dd1f819b9659c90eb3910dad2acb55b71
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-03-24 14:50:05 -0300

    feat:apaga arquivos e comentarios desnecessarios

--- tests/Unit/DeleteWorkoutTest.php
+++ tests/Unit/DeleteWorkoutTest.php
@@ -9,11 +9,7 @@ use Tests\TestCase;
 
 class DeleteWorkoutTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-
-    public function test_user_can_delete_workout(){
+       public function test_user_can_delete_workout(){
 
         $workoutCreated = Workout::factory()->create();
 
