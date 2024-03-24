f1dbdae283cf082359e3fb02b992dd509f9e61f2
Caroline Sampaio <caroline_08022@hotmail.com>
2024-03-24 19:24:16 -0300

    feat: adiciona logica base atualizacao usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,8 +12,10 @@ use App\Http\Services\User\GetAllUsersService;
 use App\Http\Services\User\PasswordGenerationService;
 use App\Http\Services\User\PasswordHashingService;
 use App\Http\Services\User\SendEmailWelcomeService;
+use App\Models\User;
 
 use App\Traits\HttpResponses;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
@@ -27,7 +29,7 @@ class UserController extends Controller
         SendEmailWelcomeService $sendEmailWelcomeService,
         CreateOneUserService $createOneUserService
     ) {
-        $body =  $request->input();
+        $body = $request->input();
 
         if ($request->hasFile('photo')) {
             $file = $createFileService->handle('photos', $request->file('photo'), $body['name']);
@@ -52,13 +54,36 @@ class UserController extends Controller
         return $users;
     }
 
-    public function update()
+    public function update(Request $request, $id, CreateFileService $createFileService,)
     {
+        $user = User::find($id);
+
+        if (!$user) {
+            return $this->error('O usuário não está cadastrado no banco de dados.', Response::HTTP_NOT_FOUND);
+        }
+
+        $body = $request->input();
+
+        $request->validate([
+            'name' => 'string|max:255|regex:/^[\p{L}\s]+$/u',
+            'email' => 'string|email|max:255|unique:users',
+            'profile_id' => 'integer|in:2,3,4',
+            'photo' => 'file|mimes:jpeg,png,jpg,gif,svg',
+        ]);
+
+        if ($request->hasFile('photo')) {
+
+            $file = $createFileService->handle('photos', $request->file('photo'), $body['name']);
+            $body['file_id'] = $file->id;
+        }
+
+        $user->update($body);
+
+        return $user;
     }
 
     public function destroy($id, DeleteOneUserService $deleteOneUserService)
     {
-
         return $deleteOneUserService->handle($id);
     }
 }
