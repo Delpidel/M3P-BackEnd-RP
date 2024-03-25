6792976f59831b2ea74c19974cfea6d20f85b3e2
Caroline Sampaio <caroline_08022@hotmail.com>
2024-03-24 21:57:29 -0300

    feat: teste admin pode atualizar foto do usuario

--- tests/Unit/AdminUpdateTest.php
+++ tests/Unit/AdminUpdateTest.php
@@ -3,7 +3,8 @@
 namespace Tests\Feature;
 
 use App\Models\User;
-
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class AdminUpdateTest extends TestCase
@@ -41,6 +42,26 @@ class AdminUpdateTest extends TestCase
         $response->assertStatus(200)->assertJson([
             ...$user->toArray(),
             'email' => 'newemail@test.com'
+        ]);
+    }
+
+    public function test_admin_can_update_user_photo()
+    {
+        $admin = User::factory()->create(['profile_id' => 1]);
+        $user = User::factory()->create(['profile_id' => 2]);
+        $token = $admin->createToken('@academia', ['update-users'])->plainTextToken;
+
+        Storage::fake('s3'); // Mock AWS S3
+
+        $body = [
+            'photo' => UploadedFile::fake()->image('new_photo.jpg')
+        ];
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)->put('/api/users/' . $user->id, $body);
+
+        $response->assertStatus(200)->assertJson([
+            ...$user->toArray(),
+            'file_id' => $response['file_id']
         ]);
     }
 }
