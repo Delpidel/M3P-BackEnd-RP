5bf1b70f7ae31432e8958e79d88d8a99355d1355
Caroline Sampaio <caroline_08022@hotmail.com>
2024-03-24 21:53:08 -0300

    feat: teste admin pode atualizar email do usuario

--- tests/Unit/AdminUpdateTest.php
+++ tests/Unit/AdminUpdateTest.php
@@ -25,4 +25,22 @@ class AdminUpdateTest extends TestCase
             'name' => 'New Name'
         ]);
     }
+
+    public function test_admin_can_update_user_email()
+    {
+        $admin = User::factory()->create(['profile_id' => 1]);
+        $user = User::factory()->create(['profile_id' => 3]);
+        $token = $admin->createToken('@academia', ['update-users'])->plainTextToken;
+
+        $body = [
+            'email' => 'newemail@test.com',
+        ];
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)->put('/api/users/' . $user->id, $body);
+
+        $response->assertStatus(200)->assertJson([
+            ...$user->toArray(),
+            'email' => 'newemail@test.com'
+        ]);
+    }
 }
