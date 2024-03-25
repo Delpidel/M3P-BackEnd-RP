0ef37ca690e2c23c7f0297829031f089f4ff4283
Caroline Sampaio <caroline_08022@hotmail.com>
2024-03-24 21:32:14 -0300

    feat: teste admin pode atualizar nome do usuario

---
+++ tests/Unit/AdminUpdateTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+
+use Tests\TestCase;
+
+class AdminUpdateTest extends TestCase
+{
+    public function test_admin_can_update_user_name()
+    {
+        $admin = User::factory()->create(['profile_id' => 1]);
+        $user = User::factory()->create(['profile_id' => 2]);
+        $token = $admin->createToken('@academia', ['update-users'])->plainTextToken;
+
+        $body = [
+            'name' => 'New Name',
+        ];
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)->put('/api/users/' . $user->id, $body);
+
+        $response->assertStatus(200)->assertJson([
+            ...$user->toArray(),
+            'name' => 'New Name'
+        ]);
+    }
+}
