877be8bb0a667234fe8792b3484ce49a8597fa45
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2024-03-25 00:23:51 -0500

    feat:adiciona test que verifica status code =200

--- tests/Unit/StudentWorkoutTest.php
+++ tests/Unit/StudentWorkoutTest.php
@@ -2,15 +2,23 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use App\Http\Controllers\WorkoutController;
+use App\Models\User;
 
 class StudentWorkoutTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     */
-    public function test_example(): void
-    {
-        $this->assertTrue(true);
-    }
+    public function testWorkoutsByStudent(): void
+    {        
+        $studentId = 6;    
+        
+        $user = User::factory()->create(['id' => $studentId]);    
+        
+        $this->actingAs($user);    
+        
+        $response = $this->get("/api/students/{$studentId}/workouts");      
+        
+        $response->assertStatus(200);
+    }    
+    
 }
