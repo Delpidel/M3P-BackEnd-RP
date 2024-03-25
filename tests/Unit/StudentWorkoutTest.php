f61f6aaed2d3fa50ef98ddca481296ae33cd4909
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2024-03-25 14:42:52 -0500

    feat: teste 2 back estrutura do array de resposta

--- tests/Unit/StudentWorkoutTest.php
+++ tests/Unit/StudentWorkoutTest.php
@@ -3,11 +3,13 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use App\Http\Controllers\WorkoutController;
 use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class StudentWorkoutTest extends TestCase
 {
+    use DatabaseTransactions;
+    
     public function testWorkoutsByStudent(): void
     {        
         $studentId = 6;    
@@ -20,5 +22,37 @@ class StudentWorkoutTest extends TestCase
         
         $response->assertStatus(200);
     }    
-    
+
+    public function testWorkoutsStudent(): void
+    {        
+        
+        $studentId = 6; 
+      
+        $user = User::factory()->create(['id' => $studentId]);         
+       
+        $this->actingAs($user);    
+
+        $response = $this->get("/api/students/{$studentId}/workouts");  
+
+        $response->assertStatus(200);
+
+        // Verificamos que la respuesta contenga los datos esperados
+        $response->assertJsonStructure([
+            'student_id',
+            'name',
+            'workouts' => [
+                '*' => [
+                    '*' => [
+                        'description',
+                        'repetitions',
+                        'weight',
+                        'break_time',
+                        'observations',
+                        'time',
+                        'created_at'
+                    ]
+                ]
+            ]
+        ]);
+    }    
 }
