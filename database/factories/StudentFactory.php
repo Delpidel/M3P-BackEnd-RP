97271931dbf39b4ee9c03ee222d8b6e4ee526b6a
joseCdia <josecdia@hotmail.com>
2024-03-24 21:47:11 -0300

    Add pasta Feature

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -4,8 +4,6 @@ namespace Database\Factories;
 
 use App\Models\Student;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class StudentFactory extends Factory
 {
