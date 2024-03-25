d0109059349f110b46a334952670ec2c5cef9a2e
Guilherme <guiduartesbs@hotmail.com>
2024-03-25 20:37:47 -0300

    feat: add meal_plans routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\StudentMealsController;
 use App\Http\Controllers\WorkoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,6 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('students', [StudentController::class, 'index']);
     Route::get('workouts', [WorkoutController::class, 'index']);
+    Route::get('students/meal_plans', [StudentMealsController::class, 'index']);
+    Route::get('students/meal_plans/{id}', [StudentMealsController::class, 'show']);
+
 });
 
 Route::post('login', [AuthController::class, 'store']);
