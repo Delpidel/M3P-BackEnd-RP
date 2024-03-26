f5bfcb23b2d86e8e8ed4782d1555741c41bd9ebd
Caroline Sampaio <caroline_08022@hotmail.com>
2024-03-25 21:50:51 -0300

    feat: adiciona tratamento com middleware form-data

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\DbTransactionMiddleware::class,
-        \AliReaza\Laravel\Request\Middleware\FormData::class
+        \App\Http\Middleware\FormData::class,
     ];
 
     /**
