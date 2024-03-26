9d2d2e8d1e8e98bfe30fb8aa669c6b7845112b21
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-03-26 18:55:01 -0300

    refactor: refatora instructorListWorkoutController para trabalhar com service.

--- app/Http/Controllers/InstructorWorkoutController.php
+++ app/Http/Controllers/InstructorWorkoutController.php
@@ -2,37 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Student;
-use App\Models\Workout;
-use Illuminate\Http\Request;
 use App\Traits\HttpResponses;
 use Symfony\Component\HttpFoundation\Response;
+use App\Http\Services\InstructorListWorkoutService;
 
 class InstructorWorkoutController extends Controller
 {
-    public function listWorkouts($id){
+    use HttpResponses;
+
+    public function listWorkouts($id, InstructorListWorkoutService $listWorkoutService)
+    {
         try {
-            $student = Student::find($id);
+            $workouts = $listWorkoutService->listWorkouts($id);
 
-            $workouts = Workout::where('student_id', $student->id)
-                ->orderBy('created_at', 'ASC')
-                ->with(['exercise' => function ($query) {
-                    $query->select('id', 'description');
-                }])->get();
-
-            $workoutsByDay = [];
-
-            $weekDays = ['SEGUNDA', 'TERÇA', 'QUARTA', 'QUINTA', 'SEXTA', 'SÁBADO', 'DOMINGO'];
-
-            foreach ($weekDays as $day) {
-                $workoutsByDay[$day] = $workouts->where('day', $day)->isEmpty() ? [] : $workouts->where('day', $day)->all();
-            }
-
-            return [
-                'student_id' => $student->id,
-                'student_name' => $student->name,
-                'workouts' => $workoutsByDay,
-            ];
+            return $workouts;
         } catch (\Exception $exception) {
             return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
