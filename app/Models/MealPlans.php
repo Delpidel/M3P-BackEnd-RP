7edebd40b10c68d9923e386542c4def86d5f6d7e
Guilherme <guiduartesbs@hotmail.com>
2024-03-28 18:58:28 -0300

    feat: add filable field in MealPlans

--- app/Models/MealPlans.php
+++ app/Models/MealPlans.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 class MealPlans extends Model
 {
 
+    protected $fillable = ['description', 'student_id'];
+
     public function mealPlansSchedule()
     {
         return $this->hasMany(MealPlansSchedule::class, 'meal_plan_id');
