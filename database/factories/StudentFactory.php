78ff055633a40bb40e823b0b1d48a478d7a59df0
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-03-29 15:03:32 -0300

    feat: cria o factory do estudante

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+class StudentFactory extends Factory
+{
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'cpf' => $this->faker->numerify('###########'),
+            'date_birth' => $this->faker->date,
+            'contact' => $this->faker->phoneNumber,
+            'cep' => $this->faker->numerify('########'),
+            'street' => $this->faker->streetName,
+            'state' => $this->faker->stateAbbr,
+            'neighborhood' => $this->faker->city,
+            'city' => $this->faker->city,
+            'number' => $this->faker->randomNumber(3),
+            'complement' => 'Apartamento ' . $this->faker->randomNumber(2), // Adicionando o complemento aos dados gerados
+        ];
+    }
+}
