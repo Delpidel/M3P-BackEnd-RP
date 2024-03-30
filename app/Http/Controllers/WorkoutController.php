2c1fc6c7912254004e6cad8ffb0dd41c65f76261
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2024-03-30 01:09:40 -0500

    refactor: join tabela workouts con tabela users_students

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -23,43 +23,45 @@ class WorkoutController extends Controller
     }
 
     public function workoutsByStudent($studentId)
-    {
-        try {
-            $studentId = auth()->id();    
-            
-            if ($studentId !== null) {
+{
+    try {
+        $studentId = auth()->id();    
+    
+        if ($studentId !== null) {
 
-                $workouts = Workout::select('workouts.*', 'students.name', 'exercises.description')
-                    ->join('students', 'workouts.student_id', '=', 'students.id')
-                    ->join('exercises', 'workouts.exercise_id', '=', 'exercises.id')
-                    ->where('workouts.student_id', $studentId)
-                    ->orderBy('created_at')
-                    ->get();
+            $workouts = Workout::select('workouts.*', 'users_students.user_id', 'exercises.description')
+                ->join('users_students', 'workouts.student_id', '=', 'users_students.student_id')
+                ->join('exercises', 'workouts.exercise_id', '=', 'exercises.id')
+                ->where('users_students.user_id', $studentId)
+                ->orderBy('workouts.created_at')
+                ->get();
+            
+            $formattingWorkouts = [];
+
+            foreach ($workouts as $workout) {
+                $formattingWorkouts['student_id'] = $workout->student_id;
+                $formattingWorkouts['user_id'] = $workout->user_id; // Agrega el id del usuario
+                $formattingWorkouts['name'] = $workout->name;
                 
-                $formattingWorkouts = [];
-    
-                foreach ($workouts as $workout) {
-                    $formattingWorkouts['student_id'] = $workout->student_id;
-                    $formattingWorkouts['name'] = $workout->name;
-                    
-                    $day = $workout->day;
-                    $formattingWorkouts['workouts'][$day][] = [
-                        'description' => $workout->description,
-                        'repetitions' => $workout->repetitions,
-                        'weight' => $workout->weight,
-                        'break_time' => $workout->break_time,
-                        'observations' => $workout->observations,
-                        'time' => $workout->time,
-                        'created_at' => $workout->created_at,
-                    ];
-                }
-    
-                return response()->json($formattingWorkouts);
-            } else {
-                return response()->json(['error' => 'Não há usuário autenticado'], 403);
+                $day = $workout->day;
+                $formattingWorkouts['workouts'][$day][] = [
+                    'description' => $workout->description,
+                    'repetitions' => $workout->repetitions,
+                    'weight' => $workout->weight,
+                    'break_time' => $workout->break_time,
+                    'observations' => $workout->observations,
+                    'time' => $workout->time,
+                    'created_at' => $workout->created_at,
+                ];
             }
-        } catch (\Exception $exception) {
-            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+
+            return response()->json($formattingWorkouts);
+        } else {
+            return response()->json(['error' => 'Não há usuário autenticado'], 403);
         }
+    } catch (\Exception $exception) {
+        return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
     }
+}
+
 }
