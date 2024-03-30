f4c36f323a4b5396e9282e903cfd268d08907c34
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-03-30 18:20:28 -0300

    refactor: ajusta testes de criação de estudante

--- tests/Unit/StudentTest.php
+++ tests/Unit/StudentTest.php
@@ -3,13 +3,14 @@
 namespace Tests\Feature;
 
 use App\Http\Services\Student\PasswordGenerationService;
-use App\Http\Services\Student\SendCredentialsStudentEmail;
-
+use App\Http\Services\Student\SendCredentialsStudentEmailService;
+use App\Mail\CredentialsStudent;
 use App\Models\Student;
 use App\Models\User;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
@@ -17,18 +18,14 @@ class StudentTest extends TestCase
 {
     public function test_recepcionist_can_create_student(): void
     {
+        Mail::fake();
 
-        // Autentica um usuário recepcionista e obtém o token de autenticação
-        $user = User::factory()->create([
-            'profile_id' => 2
-        ]);
+        $user = User::factory()->create(['profile_id' => 2]);
         $token = $user->createToken('Token recepcionista', ['create-students'])->plainTextToken;
 
         Storage::fake('s3'); // Mock AWS S3
-
         $photo = UploadedFile::fake()->image('photo.jpg');
 
-        // Simula os dados do corpo da requisição, incluindo o id para a imagem
         $student = [
             'name' => 'João da Silva',
             'email' => 'joao@example.com',
@@ -42,50 +39,31 @@ class StudentTest extends TestCase
             'city' => 'Santa cruz do sul',
             'number' => '642',
             'complement' => 'Casa amarela',
-            'photo' => $photo
         ];
 
-        // Faz uma requisição POST para o endpoint do controlador
         $response = $this->withHeader('Authorization', 'Bearer ' . $token)
-            ->post('/api/students', $student);
+            ->post('/api/students', $student + ['photo' => $photo]);
 
+        Mail::assertSent(CredentialsStudent::class, function ($mail) {
+            return $mail->hasTo('joao@example.com');
+        });
 
-        // Verifica se a resposta tem o status 201 (ou o status esperado)
-        $response->assertStatus(201)
-            ->assertJson('students', [
-                'name' => 'João da Silva',
-                'email' => 'joao@example.com',
-                'cpf' => '024.892.560-26',
-                'date_birth' => '1945-01-24',
-                'contact' => '980579171',
-                'cep' => '96810174',
-                'street' => 'Rua vinte e oito de setembro',
-                'state' => 'RS',
-                'neighborhood' => 'Centro',
-                'city' => 'Santa cruz do sul',
-                'number' => '642',
-                'complement' => 'Casa amarela',
-                'file_id' => 1,
-                'id' => 1
-            ]);
+        $response->assertStatus(201)->assertJson([...$student, 'file_id' => 1]);
     }
 
-    public function test_receptionist_token_was_generated(): void
+    public function test_non_receptionist_cannot_create_student(): void
     {
-        $receptionist = User::find(2);
-        $credentials = [
-            'email' => $receptionist->email,
-            'password' => '12345678',
-        ];
 
-        $response = $this->postJson('/api/login', $credentials);
+        $user = User::factory()->create([
+            'profile_id' => 3
+        ]);
+        $token = $user->createToken('Token', ['create-exercises'])->plainTextToken;
 
-        $response->assertStatus(200);
-    }
+        $photo = UploadedFile::fake()->image('photo.jpg');
 
-    public function test_request_body_all(): void
-    {
-        $body = [
+        Storage::fake('s3'); // Mock AWS S3
+
+        $student = [
             'name' => 'João da Silva',
             'email' => 'joao@example.com',
             'cpf' => '024.892.560-26',
@@ -97,38 +75,15 @@ class StudentTest extends TestCase
             'neighborhood' => 'Centro',
             'city' => 'Santa cruz do sul',
             'number' => '642',
+            'complement' => 'Casa amarela',
         ];
-        $request = new Request($body);
 
-        $result = $request->all();
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
+            ->post('/api/students', $student + ['photo' => $photo]);
 
-        $this->assertEquals($body, $result);
-    }
-
-    public function test_password_is_being_generated(): void
-    {
-
-        $passwordGenerationService = new PasswordGenerationService();
-
-        $password = $passwordGenerationService->handle();
-
-        $this->assertIsString($password);
-        $this->assertGreaterThanOrEqual(8, strlen($password));
-    }
-
-    public function test_credentials_are_being_sent_by_email(): void
-    {
-        $student = new Student([
-            'name' => 'João da Silva',
-            'email' => 'joao@example.com',
-        ]);
-
-        $password = 'senha123';
-
-        $emailServiceMock = \Mockery::mock(SendCredentialsStudentEmail::class);
-
-        $emailServiceMock->shouldReceive('handle')->with($student, $password)->once();
-
-        $emailServiceMock->handle($student, $password);
+        $response->assertStatus(403)
+            ->assertJsonFragment([
+                'message' => 'Acesso negado. Você não possui permissão para executar esta ação.'
+            ]);
     }
 }
