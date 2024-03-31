7fa719910f0caec3908e8d5f1421ecb1214b706a
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2024-03-30 21:40:07 -0500

    feat:adiciona os testes unitarios

---
+++ tests/Unit/StudentWorkoutTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class StudentWorkoutTest extends TestCase
+{
+    use DatabaseTransactions;
+    
+    public function testWorkoutsByStudent(): void
+    {        
+        $studentId = 6;    
+        
+        $user = User::factory()->create(['id' => $studentId]);    
+        
+        $this->actingAs($user);    
+        
+        $response = $this->get("/api/students/{$studentId}/workouts");      
+        
+        $response->assertStatus(200);
+    }    
+
+    public function testWorkoutsByStudentFormat(): void
+    {        
+        
+        $studentId = 6; 
+      
+        $user = User::factory()->create(['id' => $studentId]);         
+       
+        $this->actingAs($user);    
+
+        $response = $this->get("/api/students/{$studentId}/workouts");  
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'student_id',
+            'name',
+            'workouts' => [
+                '*' => [
+                    '*' => [
+                        'description',
+                        'repetitions',
+                        'weight',
+                        'break_time',
+                        'observations',
+                        'time',
+                        'created_at'
+                    ]
+                ]
+            ]
+        ]);
+    } 
+
+    public function test_student_can_list_their_own_workouts()
+    {
+        $student = User::factory()->create(['profile_id' => 5]);
+        $token = $student->createToken('@academia', ['get-workout'])->plainTextToken;
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)->get('/api/students/' . $student->id . '/workouts');
+        $response->assertStatus(200);
+    }
+
+    public function test_student_can_not_list_workouts_from_another_student()
+    {
+        $student = User::factory()->create(['profile_id' => 5]);
+        $anotherStudent = User::factory()->create(['profile_id' => 4]);
+        $token = $student->createToken('@academia', ['get-workout'])->plainTextToken;
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)->get('/api/students/' . $anotherStudent->id . '/workouts');
+        $response->assertStatus(403);
+    }
+
+}
+
