55e3df77429939cfe6d5120b0790c94c98722eb0
Guilherme <guiduartesbs@hotmail.com>
2024-03-31 21:13:25 -0300

    refactor : get the student_id from user_id

--- app/Http/Repositories/StudentMealsRepository.php
+++ app/Http/Repositories/StudentMealsRepository.php
@@ -4,17 +4,21 @@ namespace App\Http\Repositories;
 use App\Interfaces\StudentMealsRepositoryInterface;
 use App\Models\File;
 use App\Models\MealPlans;
+use App\Models\UserStudent;
 
 class StudentMealsRepository implements StudentMealsRepositoryInterface {
 
     public function getPlans($userId) {
-        return MealPlans::query()->where('student_id', $userId)->get();
+
+        $studentId = UserStudent::where('user_id', $userId)->value('student_id');
+        return MealPlans::query()->where('student_id', $studentId)->get();
 
     }
 
     public function getSchedule($id, $userId)
     {
-        return MealPlans::query()->where('student_id', $userId)->where('id', $id)->with(['mealPlansSchedule' => function ($query) {
+        $studentId = UserStudent::where('user_id', $userId)->value('student_id');
+        return MealPlans::query()->where('student_id', $studentId)->where('id', $id)->with(['mealPlansSchedule' => function ($query) {
             $query->orderBy('hour');
         }])->first();
     }
