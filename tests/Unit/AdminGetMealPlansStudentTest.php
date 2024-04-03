d3c615e4b9695391a61cc4a116dd875febd7a9d3
Guilherme <guiduartesbs@hotmail.com>
2024-04-03 19:27:55 -0300

    refactor: Improve indentation and remove unused things.

--- tests/Unit/AdminGetMealPlansStudentTest.php
+++ tests/Unit/AdminGetMealPlansStudentTest.php
@@ -6,8 +6,6 @@ use App\Models\MealPlans;
 use App\Models\Student;
 use App\Models\User;
 use App\Models\UserStudent;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AdminGetMealPlansStudentTest extends TestCase
@@ -15,9 +13,11 @@ class AdminGetMealPlansStudentTest extends TestCase
 
     public function test_student_can_get_meal_plans()
     {
-        $user = User::factory()->create(['profile_id' => 1]);
+        $user = User::factory()->create(['profile_id' => 5]);
         $token = $user->createToken('@academia', ['get-meal-plans'])->plainTextToken;
+
         $response = $this->withHeader('Authorization', 'Bearer ' . $token)->get('/api/students/meal_plans');
+
         $response->assertStatus(200)->assertJsonStructure([
             '*' => [
                 'description',
@@ -27,9 +27,10 @@ class AdminGetMealPlansStudentTest extends TestCase
             ]
         ]);
     }
+
     public function test_student_can_get_meal_plans_schedule()
     {
-        $user = User::factory()->create(['profile_id' => 1]);
+        $user = User::factory()->create(['profile_id' => 5]);
         $student = Student::create([
             'name' => 'Douglas da Silva',
             'email' => 'joao@example.com',
@@ -43,20 +44,17 @@ class AdminGetMealPlansStudentTest extends TestCase
             'city' => 'Santa cruz do sul',
             'number' => '642',
         ]);
-
-        $UserStudent = UserStudent::create([
+        UserStudent::create([
             'user_id' => $user->id,
             'student_id' => $student->id,
         ]);
-
         $studentId = UserStudent::where('user_id', $user->id)->value('student_id');
-
         $mealPlan = MealPlans::create(['description' => 'emagrecimento', 'student_id' => $studentId]);
         $mealPlanId = $mealPlan->id;
-
-
         $token = $user->createToken('@academia', ['get-meal-plans'])->plainTextToken;
+
         $response = $this->withHeader('Authorization', 'Bearer ' . $token)->get('/api/students/meal_plans/' . $mealPlanId);
+
         $response->assertStatus(200)->assertJsonStructure([
             'student_id',
             'student_name',
@@ -72,25 +70,23 @@ class AdminGetMealPlansStudentTest extends TestCase
         ]);
     }
 
-    public function test_can_not_view_meal_plans_if_the_user_is_not_logged ()
+    public function test_can_not_view_meal_plans_if_the_user_is_not_logged()
     {
-        $user = User::factory()->create(['profile_id' => 1]);
-        $token = '';
-        $response = $this->withHeader('Authorization', 'Bearer ' . $token)->get('/api/students/meal_plans');
+        $response = $this->withHeader('Authorization', 'Bearer ' . '')->get('/api/students/meal_plans');
+
         $response->assertStatus(500);
     }
-    public function test_others_user_can_not_view_meal_plan_schedule_from_other_student ()
+
+    public function test_others_user_can_not_view_meal_plan_schedule_from_other_student()
     {
-        $user1 = User::factory()->create(['profile_id' => 1]);
-        $user2 = User::factory()->create(['profile_id' => 1]);
+        $user1 = User::factory()->create(['profile_id' => 5]);
+        $user2 = User::factory()->create(['profile_id' => 5]);
         $mealPlan = MealPlans::create(['description' => 'emagrecimento', 'student_id' => $user1->id]);
         $mealPlanId = $mealPlan->id;
         $token = $user2->createToken('@academia', ['get-meal-plans'])->plainTextToken;
+
         $response = $this->withHeader('Authorization', 'Bearer ' . $token)->get('/api/students/meal_plans/' . $mealPlanId);
+
         $response->assertStatus(404);
-
-
     }
-
-
 }
