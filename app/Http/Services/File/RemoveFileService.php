f6beebfa915a5b82aef12cb145fd71913c7462b5
Caroline Sampaio <caroline_08022@hotmail.com>
2024-04-03 13:23:51 -0300

    feat: adiciona logica pare remocao imagem do banco
    remove da tabela file se for apagada na atualizacao

--- app/Http/Services/File/RemoveFileService.php
+++ app/Http/Services/File/RemoveFileService.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Services\File;
 
+use App\Http\Repositories\FileRepository;
 use Illuminate\Support\Facades\Storage;
 
 class RemoveFileService
 {
-    public function handle($fileUrl)
+    private $fileRepository;
+
+    public function __construct(FileRepository $fileRepository)
+    {
+        $this->fileRepository = $fileRepository;
+    }
+
+    public function handle($fileUrl, $fileId)
     {
         $filePath = parse_url($fileUrl);
         Storage::disk('s3')->delete($filePath);
+
+        $this->fileRepository->delete($fileId);
     }
 }
