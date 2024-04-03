cb7922d9680c236db0ab13427019fa3c69f08117
ThiagoRiosB <123420372+ThiagoRiosB@users.noreply.github.com>
2024-04-02 22:19:35 -0300

    Ajustei a migration para receber as 4 imagens e as medidas do estudante.

--- database/migrations/2024_03_20_103738_create_avaliations_table.php
+++ database/migrations/2024_03_20_103738_create_avaliations_table.php
@@ -19,11 +19,32 @@ class CreateAvaliationsTable extends Migration
             $table->float('height');
             $table->text('observations_to_student')->nullable();
             $table->text('observations_to_nutritionist')->nullable();
-            $table->unsignedBigInteger('file_id');
-            $table->foreign('file_id')->references('id')->on('files');
-            $table->json('measures');
-            $table->timestamps();
 
+            $table->unsignedBigInteger('back');
+            $table->foreign('back')->references('id')->on('files');
+            $table->unsignedBigInteger('front');
+            $table->foreign('front')->references('id')->on('files');
+            $table->unsignedBigInteger('left');
+            $table->foreign('left')->references('id')->on('files');
+            $table->unsignedBigInteger('right');
+            $table->foreign('right')->references('id')->on('files');
+
+            $table->float('torax');
+            $table->float('braco_direito');
+            $table->float('braco_esquerdo');
+            $table->float('cintura');
+            $table->float('antebraco_direito');
+            $table->float('antebraco_esquerdo');
+            $table->float('abdomen');
+            $table->float('coxa_direita');
+            $table->float('coxa_esquerda');
+            $table->float('quadril');
+            $table->float('panturrilha_direita');
+            $table->float('panturrilha_esquerda');
+            $table->float('punho');
+            $table->float('biceps_femoral_direito');
+            $table->float('biceps_femoral_esquerdo');
+            $table->timestamps();
 
         });
     }
