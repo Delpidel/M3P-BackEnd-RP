ac8764e336d043c40469a12e9a479c47d3d9e634
Guimariane <ingrid.mariane.araujo@gmail.com>
2024-04-03 03:06:37 -0300

    Edição na função export e no anexo em pdf para ler as informações solicitadas

--- app/Http/Controllers/AvaliationController.php
+++ app/Http/Controllers/AvaliationController.php
@@ -11,8 +11,8 @@ class AvaliationController extends Controller
     {
         try {
 
-            $students = Avaliation::all();
-            return $students;
+            $avaliation = Avaliation::all();
+            return $avaliation;
         } catch (\Exception $exception) {
             return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
 
