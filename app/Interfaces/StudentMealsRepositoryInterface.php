da0fdafe623d0543b75f437cffde70bd93a7065c
Guilherme <guiduartesbs@hotmail.com>
2024-04-03 19:25:57 -0300

    feat: add get studentId function in interface

--- app/Interfaces/StudentMealsRepositoryInterface.php
+++ app/Interfaces/StudentMealsRepositoryInterface.php
@@ -4,5 +4,6 @@ namespace App\Interfaces;
 
 interface StudentMealsRepositoryInterface {
     public function getPlans($userId);
+    public function getStudentId($userId);
     public function getSchedule($id, $userId);
 }
