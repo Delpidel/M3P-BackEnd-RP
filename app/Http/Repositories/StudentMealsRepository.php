996deaf449c955d6f24734a8e0a441b0ea8f6969
Guilherme <guiduartesbs@hotmail.com>
2024-04-03 19:24:15 -0300

    refactor: add function getStudentId

--- app/Http/Repositories/StudentMealsRepository.php
+++ app/Http/Repositories/StudentMealsRepository.php
@@ -1,27 +1,32 @@
 <?php
 
 namespace App\Http\Repositories;
+
 use App\Interfaces\StudentMealsRepositoryInterface;
 use App\Models\File;
 use App\Models\MealPlans;
 use App\Models\UserStudent;
 
-class StudentMealsRepository implements StudentMealsRepositoryInterface {
+class StudentMealsRepository implements StudentMealsRepositoryInterface
+{
 
-    public function getPlans($userId) {
-
+    public function getStudentId($userId)
+    {
         $studentId = UserStudent::where('user_id', $userId)->value('student_id');
-        return MealPlans::query()->where('student_id', $studentId)->get();
+        return $studentId;
+    }
 
+    public function getPlans($userId)
+    {
+        $studentId = $this->getStudentId($userId);
+        return MealPlans::query()->where('student_id', $studentId)->get();
     }
 
     public function getSchedule($id, $userId)
     {
-        $studentId = UserStudent::where('user_id', $userId)->value('student_id');
+        $studentId = $this->getStudentId($userId);
         return MealPlans::query()->where('student_id', $studentId)->where('id', $id)->with(['mealPlansSchedule' => function ($query) {
             $query->orderBy('hour');
         }])->first();
     }
 }
-
-
